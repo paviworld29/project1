68b784a6986348723facef87240ac2ad61552018
clarigoweb <lavinagidwani-sdm>
2024-07-13 12:28:50 +0530

    newd

---
+++ src/sub.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const sub = () => {
+  return (
+    <View>
+      <Text>sub</Text>
+    </View>
+  )
+}
+
+export default sub
