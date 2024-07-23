596bd32a9e397400ab6af1ade93099d52246f401
clarigoweb <lavinagidwani-sdm>
2024-07-23 08:15:16 +0530

    f

---
+++ App.js
@@ -0,0 +1,51 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <ScrollView>
+      <Text style={{fontSize:30}}>Hello react native</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      <Text style={{fontSize:30}}>like,share and subscribe</Text>
+      </ScrollView>
+      
+    </View>
+  )
+}
+
+export default App
